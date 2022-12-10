562b364d61e35ddc65c6ece249b6b965e5ce6832
Guillermo Farías <guillermodanilo08@gmail.com>
2022-12-10 12:28:09 -0300

    add command test

--- app/Console/Commands/test.php
+++ app/Console/Commands/test.php
@@ -34,21 +34,6 @@ class test extends Command
         $ip = "192.168.2.251";
         $port = 4370;
 
-        $zk = new ZKLibrary($ip, $port);
-        $zk->connect();
-        $this->warn($zk->getSerialNumber());
-        $this->warn($zk->getFirmwareVersion());
-        $this->warn($zk->getWorkCode());
-
-        $start_time = microtime(true);
-        $atendance = $zk->getAttendance();
-        $end_time = microtime(true);
-        $execution_time = ($end_time - $start_time);
-        echo "1 - Time = " . $execution_time . " sec";
-
-        // $this->warn("Total: " . count($atendance));
-
-
         $options = [
             'ip' => $ip,
             'internal_id' => 1,         // 1 by default.
@@ -72,12 +57,15 @@ class test extends Command
         $att_logs = $tad->get_att_log();
         $end_time = microtime(true);
         $execution_time = ($end_time - $start_time);
-        echo "2 - Time = " . $execution_time . " sec";
+        echo "Time = " . $execution_time . " sec " . PHP_EOL;
 
-        // Now, you want filter the resulset to get att logs between '2014-01-10' and '2014-03-20'.
-        // $filtered_att_logs = $att_logs->filter_by_date(
-        //     ['start' => '2022-12-09', 'end' => '2022-12-09']
-        // );
+        $start_time = microtime(true);
+        $filtered_att_logs = $att_logs->filter_by_date(
+            ['start' => '2022-12-09', 'end' => '2022-12-09']
+        );
+        $end_time = microtime(true);
+        $execution_time = ($end_time - $start_time);
+        echo "Time to filter = " . $execution_time . " sec " . PHP_EOL;
 
 
         // print_r($filtered_att_logs->to_array());
